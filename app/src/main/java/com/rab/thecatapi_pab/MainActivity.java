dcfd3a7830269765bcb4764e044aa4e145408265
ihsbramn <abdurahmanihsan25@gmail.com>
2021-12-25 14:21:34 +0700

    home fragment binding

--- app/src/main/java/com/rab/thecatapi_pab/MainActivity.java
+++ app/src/main/java/com/rab/thecatapi_pab/MainActivity.java
@@ -32,8 +32,6 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG1 = "HomeFragment";
-
     private ActivityMainBinding binding;
 
     @Override
@@ -53,66 +51,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        // Home Act
-
-        //        click random cat
-        findViewById(R.id.RandomCatCall).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//        okhttp client
-                OkHttpClient client = new OkHttpClient();
-//        url
-                String BASE_URL = "https://api.thecatapi.com/v1/";
-                Request randomCat = new Request.Builder()
-                        .url(BASE_URL + "images/search")
-                        .addHeader("x-api-key","b7dfc536-2e87-40ac-ba6f-d3f14154ebee")
-                        .build();
-
-                client.newCall(randomCat).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                        Log.d(TAG1, "onFailure: ERROR!");
-                        e.printStackTrace();
-                    }
-                    @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        Log.d(TAG1, "onResponse: Got Response");
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if (response.isSuccessful()){
-                                    String json = null;
-                                    try {
-                                        json = response.body().string();
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-//                                    get string from json string
-                                    try {
-                                        JSONArray arrayJSON_RESP = new JSONArray(json);
-                                        JSONObject objectJSON_RESP = arrayJSON_RESP.getJSONObject(0);
-                                        String url = objectJSON_RESP.getString("url");
-////                                        set text
-//                                        TextView textView = findViewById(R.id.RandomCatTextJSON);
-//                                        textView.setText(url);
-//                                        set img
-                                        ImageView imageView = findViewById(R.id.imageContainer);
-                                        Picasso.get().load(url).into(imageView);
-//                                        debug logger
-                                        Log.d(TAG1, "onResponse: text and img set on "+url);
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-
-
-                                }
-                            }
-                        });
-
-                    }
-                });
-            }
-        });
     }
 
 }
--- app/src/main/java/com/rab/thecatapi_pab/ui/home/HomeFragment.java
+++ app/src/main/java/com/rab/thecatapi_pab/ui/home/HomeFragment.java
@@ -1,10 +1,17 @@
 package com.rab.thecatapi_pab.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +21,23 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.rab.thecatapi_pab.R;
 import com.rab.thecatapi_pab.databinding.FragmentHomeBinding;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "HomeFragment";
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
@@ -28,11 +49,83 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+//        binding text
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+//        binding random cat call
+        binding.RandomCatCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                OkHttpClient
+                OkHttpClient client = new OkHttpClient();
+//                endpoint
+                String BASE_URL = "https://api.thecatapi.com/v1/";
+                Request randomCat = new Request.Builder()
+                        .url(BASE_URL + "images/search")
+                        .addHeader("x-api-key", "b7dfc536-2e87-40ac-ba6f-d3f14154ebee")
+                        .build();
+
+                client.newCall(randomCat).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+//                        debug logger
+                        Log.d(TAG, "onFailure: ERROR!");
+                        e.printStackTrace();
+                    }
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+//                        debug logger
+                        Log.d(TAG, "onResponse: Got Response");
+                        if (response.isSuccessful()) {
+                            String json = null;
+                            try {
+                                json = response.body().string();
+                            } catch (IOException e) {
+//                                debug logger
+                                e.printStackTrace();
+                            }
+                            try {
+//                                get url string
+                                JSONArray arrayJSON_RESP = new JSONArray(json);
+                                JSONObject objectJSON_RESP = arrayJSON_RESP.getJSONObject(0);
+                                String url = objectJSON_RESP.getString("url");
+
+//                                Toast Property
+                                Context context = getContext();
+                                int duration = Toast.LENGTH_SHORT;
+                                String text = ("Meow ! "+url);
+
+//                                img binding
+                                ImageView imageView = binding.imageContainer;
+
+//                                UiHandler
+                                Handler uiHandler = new Handler(Looper.getMainLooper());
+                                uiHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Picasso
+                                                .get()
+                                                .load(url)
+                                                .into(imageView);
+
+                                        Toast toast = Toast.makeText(context, text, duration);
+                                        toast.show();
+                                    }
+                                });
+
+                            } catch (JSONException e) {
+//                                debug logger
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
             }
         });
         return root;
